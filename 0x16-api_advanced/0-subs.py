0a8c879e0fec5be8dfaa74c6936c7b5d6eb9d76d
DekunleJr <adekunlesa@gmail.com>
2023-05-09 23:23:26 +0000

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,5 +10,5 @@ def number_of_subscribers(subreddit):
 		return 0
 	r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-Agent': '0x16-api_advanced:project:\
 v0.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-	ubs = r.get("data", {}).get("subscribers", 0)
+	subs = r.get("data", {}).get("subscribers", 0)
 	return subs

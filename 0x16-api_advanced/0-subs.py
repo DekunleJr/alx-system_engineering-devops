9a669195972f375bd1ccc2c532d2663b5b464e13
Dekunle <adekunlesa10@gmail.com>
2023-05-09 12:56:45 +0100

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ def number_of_subscribers(subreddit):
     # function that queries the Reddit API and returns the number of
     # subscribers for a given subreddit
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get('data')
-        return data.get('subscribers')
+        return data.gitget('subscribers')
     else:
         return 0
